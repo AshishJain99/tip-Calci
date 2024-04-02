c3c47460d7f64feca31cde635c121350b5487bd1
Ashish Jain <imjainashish@gmail.com>
2024-04-02 22:20:31 +0530

    Initial Commit

---
+++ tip-Calci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-Calci
+//
+//  Created by Ashish Jain on 02/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
